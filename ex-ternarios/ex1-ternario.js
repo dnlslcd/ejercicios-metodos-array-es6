9a7e2c433441a452746305da61552d666cc86bc6
dnlslcd <danielsalcedoq@gmail.com>
2024-07-03 21:35:40 +0200

    ex1-ternario actualizado

--- ex-ternarios/ex1-ternario.js
+++ ex-ternarios/ex1-ternario.js
@@ -3,14 +3,16 @@
  */
 
 let edad = 21;
-let mensaje;
+let mensaje; 
 
-if (edad >= 18) {
-    mensaje = "Puedes pasar";
-}
+mensaje = (edad >= 18) ? "Puedes pasar" : "No puedes pasar";
 
-else {
-    mensaje = "No puedes pasar";
-}
+// if (edad >= 18) {
+//     mensaje = "Puedes pasar";
+// }
+
+// else {
+//     mensaje = "No puedes pasar";
+// }
 
 console.log(mensaje);
